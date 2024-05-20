166263a3b66e87f232c7feeda3bee7f9c1e8e585
Jonas Javier Encarnacion <hachijonas@hotmail.com>
2024-05-20 11:06:13 -0400

    Delete frontend directory

--- frontend/src/components/LazyImage.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/LazyImage.jsx
-import React from 'react';
-
-const LazyImage = ({ src, alt }) => (
-  <img src={src} alt={alt} loading="lazy" className="img-fluid" />
-);
-
-export default LazyImage;
