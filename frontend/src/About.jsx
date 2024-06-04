bc2761f8a37b0bd5c14365d5a99f63cd419504a9
Jonas Javier Encarnacion <hachijonas@hotmail.com>
2024-06-04 00:16:09 -0400

    Feature component

--- frontend/src/About.jsx
+++ frontend/src/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import About_hero from "./components/About-hero";
-import AboutMe2 from "./components/AboutMe2"
+import Feature from "./components/Feature"
 
 
 
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <>
       <About_hero />
-      <AboutMe2 />
+      <Feature />
     </>
   );
 };
---
+++ frontend/src/components/Feature.jsx
@@ -0,0 +1,58 @@
+import React, { memo } from 'react';
+import { useInView } from 'react-intersection-observer';
+import CountUp from 'react-countup';
+import '../assets/styles/Feature.css';
+
+
+
+const Feature = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <div className="container-xxl py-5 dark-mode" ref={ref}>
+      <div className={`container ${inView ? 'animate__animated animate__fadeInUp' : ''}`}>
+        <div
+          className="text-center mx-auto"
+          style={{ maxWidth: '500px' }}
+        >
+          <p className="text-primary text-uppercase mb-2">Why Choose Us!</p>
+          <h1 className="display-6 mb-5 text-light">The Leading Photo Studio In The Country</h1>
+        </div>
+        <div className="row g-3">
+          <div className="col-lg-4 col-md-6 pt-lg-5">
+            <div className="fact-item dark-mode-item text-center h-100 p-5">
+              <h1 className="display-2 text-primary mb-3">
+                {inView && <CountUp end={50} duration={2} prefix="+" />}
+              </h1>
+              <h4 className="mb-3 text-light">Happy Clients</h4>
+              <span className="text-light">Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo</span>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6">
+            <div className="fact-item dark-mode-item text-center h-100 p-5">
+              <h1 className="display-2 text-primary mb-3">
+                {inView && <CountUp end={5} duration={4} prefix="+" />}
+              </h1>
+              <h4 className="mb-3 text-light">Years Experience</h4>
+              <span className="text-light">Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo</span>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 pt-lg-5">
+            <div className="fact-item dark-mode-item text-center h-100 p-5">
+              <h1 className="display-2 text-primary mb-3">
+                {inView && <CountUp end={500} duration={2} prefix="+" />}
+              </h1>
+              <h4 className="mb-3 text-light">Portfolio Photos</h4>
+              <span className="text-light">Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(Feature);
